1a66feb85ba6fe22cd8742b52215bf67ab50f59d
Sadik Al Jarif <sadikaljarif05@gmail.com>
2025-10-06 18:28:22 +0600

    Add files via upload

---
+++ sql/scripts/1.select_from.sql
@@ -0,0 +1,14 @@
+-- this is a comment
+/* hello this is a sql program */
+
+select * from customers
+
+-- retrive all orders data
+
+select * from orders
+
+
+-- retrive each customers name ,country and score
+
+select first_name,country,score from customers
+
